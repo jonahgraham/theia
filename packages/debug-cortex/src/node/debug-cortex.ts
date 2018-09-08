d79ed2b6fe498927e0b1b9b588d1db78cfb8af62
Jonah Graham <jonah@kichwacoders.com>
2018-09-08 14:09:04 +0100

    Add Cortex Debug

---
+++ packages/debug-cortex/src/node/debug-cortex.ts
@@ -0,0 +1,64 @@
+/********************************************************************************
+ * Copyright (C) 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+const path = require('path');
+const packageJson = require('../../package.json');
+const debugAdapterDir = packageJson['debugAdapter']['dir'];
+
+import { injectable } from 'inversify';
+import { DebugConfiguration } from '@theia/debug/lib/common/debug-common';
+import { DebugAdapterContribution, DebugAdapterExecutable } from '@theia/debug/lib/node/debug-model';
+
+@injectable()
+export class CortexDebugAdapterContribution implements DebugAdapterContribution {
+    readonly debugType = 'cortex-debug';
+
+    provideDebugConfigurations = [{
+        type: this.debugType,
+        name: 'Launch Cortex Debug',
+        breakpoints: { filePatterns: ['[.]c$'] },
+        cwd: '${workspaceRoot}',
+        executable: './Debug/hello_world.elf',
+        request: 'launch',
+        servertype: 'jlink',
+        device: 'R7FS5D57C'
+    }];
+
+    resolveDebugConfiguration(config: DebugConfiguration): DebugConfiguration {
+        config.breakpoints = { filePatterns: ['[.]c$'] };
+
+        if (!config.request) {
+            throw new Error('Debug request type is not provided.');
+        }
+
+        switch (config.request) {
+            case 'launch': this.validateLaunchConfig(config);
+        }
+
+        return config;
+    }
+
+    provideDebugAdapterExecutable(config: DebugConfiguration): DebugAdapterExecutable {
+        const program = path.join(__dirname, `../../${debugAdapterDir}/out/src/gdb.js`);
+        return {
+            program,
+            runtime: 'node'
+        };
+    }
+
+    private validateLaunchConfig(config: DebugConfiguration) {
+    }
+}
